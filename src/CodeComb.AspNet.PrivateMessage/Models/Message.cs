52eb005c1a084e4c8b8818a8c9eb8260d708f8e0
あまみや ゆうこ <1@1234.sh>
2015-12-05 17:53:19 +0800

    Init

---
+++ src/CodeComb.AspNet.PrivateMessage/Models/Message.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CodeComb.AspNet.PrivateMessage.Models
+{
+    [Table("AspNetPrivateMessages")]
+    public class Message<TUser, TKey>
+    {
+        public TKey Id { get; set; }
+
+        [MaxLength(256)]
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        [ForeignKey("Sender")]
+        public TKey? SenderId { get; set; }
+
+        public virtual TUser Sender { get; set; }
+
+        [ForeignKey("Receiver")]
+        public TKey? ReceiverId { get; set; }
+
+        public virtual TUser Receiver { get; set; }
+
+        public DateTime Time { get; set; }
+    }
+}
